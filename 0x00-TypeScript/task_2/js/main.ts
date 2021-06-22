088d40c14d8997e74be05fc21287aabc6d989bcf
FouedDadi <foued.dadii@gmail.com>
2021-06-22 23:56:47 +0100

    adding two function isDirector and executeWork

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -41,11 +41,15 @@ export const createEmployee = (salary: number | string): Director | Teacher => {
   return new Director();
 };
 
-export function isDirector(employee: Teacher | Director): employee is Director {
+export function isDirector(
+  employee: TeacherInterface | DirectorInterface
+): employee is Director {
   return employee instanceof Director;
 }
 
-export function executeWork(employee: Teacher | Director): string {
+export function executeWork(
+  employee: TeacherInterface | DirectorInterface
+): string {
   if (isDirector(employee)) return employee.workDirectorTasks();
   return employee.workTeacherTasks();
 }
