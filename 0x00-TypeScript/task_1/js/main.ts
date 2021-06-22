9b19e5dc6baa3571409b259eecf1d05a7b672575
FouedDadi <foued.dadii@gmail.com>
2021-06-22 23:30:23 +0100

    adding class named StudentClass

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -22,25 +22,3 @@ export const printTeacher: printTeacherFunction = (
 ): string => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
-
-interface StudentClassConstructor {
-  new (firstName: string, lastName: string): StudentClassInterface;
-}
-interface StudentClassInterface {
-  firstName: string;
-  lastName: string;
-}
-class StudentClass implements StudentClassInterface {
-  firstName: string;
-  lastName: string;
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-  workOnHomework() {
-    return 'Currently working';
-  }
-  displayName() {
-    return this.firstName;
-  }
-}
