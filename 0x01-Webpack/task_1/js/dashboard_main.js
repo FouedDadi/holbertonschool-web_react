3b454ec56ad535027def5c58d992201527275871
FouedDadi <foued.dadii@gmail.com>
2021-06-24 21:59:25 +0100

    adding the main js file

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,18 +1,17 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-var btn = document.createElement('<button>Click here to get started</button>');
-var p = document.createElement('<p id="count"></p>');
 $('body').append('Holberton Dashboard');
 $('body').append('Dashboard data for the students');
-$('body').append(btn);
-$('body').append(p);
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
 $('body').append('Copyright - Holberton School');
 
-let count = 0;
 const updateCounter = function () {
-  count += 1;
-  const ct = document.getElementById('count');
-  ct.text(`${count} clicks on the button`);
+  var count = 0;
+  $('button').onclick = function () {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  };
 };
-btn.on('click', _.debounce(updateCounter, 500));
+$('button').on('click', _.debounce(updateCounter, 500));
