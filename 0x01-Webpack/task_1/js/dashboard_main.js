9059b7ee11d742c52975244f1470e4ad32e81798
FouedDadi <foued.dadii@gmail.com>
2021-06-24 22:00:03 +0100

    adding the main js file

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,18 @@
 import $ from 'jquery';
-import _ from 'lodash';
+const _ = require('lodash');
 
-$('body').append('Holberton Dashboard');
-$('body').append('Dashboard data for the students');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('Copyright - Holberton School');
+const p1 = $('<p></p>').text('Holberton Dashboard');
+const p2 = $('<p></p>').text('Dashboard data for the students');
+const button = $('<button></button>').text('Click here to get started');
+const p3 = $('<p></p>').prop('id', 'count');
+const p4 = $('<p></p>').text('Copyright - Holberton School');
 
-const updateCounter = function () {
-  var count = 0;
-  $('button').onclick = function () {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
-  };
+$('body').append(p1, p2, button, p3, p4);
+
+let count = 0;
+const updateCounter = () => {
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
 };
+
 $('button').on('click', _.debounce(updateCounter, 500));
