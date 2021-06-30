a1bd9077f8de72eabde17f68d8cdf8715be4cc2c
FouedDadi <foued.dadii@gmail.com>
2021-06-30 23:48:31 +0100

    installing babel

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -33,6 +33,11 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
     ],
   },
 };
