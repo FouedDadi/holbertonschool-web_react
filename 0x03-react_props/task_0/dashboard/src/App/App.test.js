439cb4adb80f2f8ab1916bd7708fc98cd8b8b896
FouedDadi <foued.dadii@gmail.com>
2021-07-04 21:17:06 +0100

    adding files for header footer and login components

--- 0x03-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x03-react_props/task_0/dashboard/src/App/App.test.js
@@ -2,6 +2,8 @@ import App from './App';
 import { shallow } from 'enzyme';
 import React from 'react';
 
+const wrapper = shallow(<App />);
+
 test('checkin crashing issue', function () {
   shallow(<App />);
 });
