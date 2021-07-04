f61e1b0210ae2033398e3b6cd978f9bee2decb6e
FouedDadi <foued.dadii@gmail.com>
2021-07-04 21:18:39 +0100

    adding configuration files

---
+++ 0x03-react_props/task_0/dashboard/config/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('./dist'),
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve('./dist'),
+    compress: true,
+    port: 3000,
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.jpg$/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+};
