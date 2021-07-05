04894b1ab21a44c2b0a12e7507aa7544fb4e8f41
FouedDadi <foued.dadii@gmail.com>
2021-07-05 22:06:50 +0100

    fixing img

--- 0x03-react_props/task_0/dashboard/src/Header/Header.js
+++ 0x03-react_props/task_0/dashboard/src/Header/Header.js
@@ -1,18 +1,20 @@
-import './Header.css';
 import React from 'react';
+import './Header.css';
+import logo from '../assets/logo.jpg';
 
 function Header() {
   return (
     <div className='App'>
       <header
-        className='App-header'
+        className='header'
         style={{
           position: 'fixed',
-          top: '15%',
+          top: '20%',
           width: '100%',
-          height: '30%',
+          height: '25%',
         }}
       >
+        <img src={logo} className='App-logo' alt='logo' />
         <h1>School dashboard</h1>
       </header>
     </div>
