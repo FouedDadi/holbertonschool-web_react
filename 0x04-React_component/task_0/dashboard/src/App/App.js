0e0f592894cc7ef034c3b57d66c8ace176bd8841
FouedDadi <foued.dadii@gmail.com>
2021-07-12 15:58:34 +0100

    adding all files

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -8,32 +8,27 @@ import CourseList from '../CourseList/CourseList';
 import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
 
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+const listNotifications = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: htmlObj },
+];
 class App extends Component {
-  listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
-  ];
-  listNotifications = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: getLatestNotification },
-  ];
-
   render() {
-    const { isLoggedIn } = this.props;
     const { listNotifications } = this.listNotifications;
+    const { isLoggedIn } = this.props.isLoggedIn;
     return (
       <>
         <Notifications listNotifications={listNotifications} />
         <div className='App'>
           <Header />
           <hr />
-          {isLoggedIn ? (
-            <CourseList listCourses={this.listCourses} />
-          ) : (
-            <Login />
-          )}
+          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
           <hr />
           <Footer />
         </div>
