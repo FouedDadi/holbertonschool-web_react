6d8c9cc1701e9b79b0ba4a032002a0f5d346af66
FouedDadi <foued.dadii@gmail.com>
2021-07-27 20:45:54 +0100

    use Map with object

--- 0x07-React_Immutable/1-map.js
+++ 0x07-React_Immutable/1-map.js
@@ -1,5 +1,7 @@
 import { Map } from 'immutable';
 
-const getImmutableObject = (object) => Map(object);
+const getImmutableObject = function (object) {
+  Map(object);
+};
 
 export default getImmutableObject;
